cd15b774f0e9f80bf46fec7a11f3110d8e22b86f
Havy <hnakalyowa29@gmail.com>
2025-09-16 14:54:43 +0300

    final file

--- assignment.py
+++ assignment.py
@@ -21,9 +21,55 @@ print(book1.describe())
 print(book2.describe()) 
 print(book3.describe())
 
+
+
+
 #MY MINI PROJECT ASSIGNMENT
+class Hostel:
+    def __init__(self,name,location):
+        self.name = name
+        self.location = location
+        self.visits = []   # list to keep visits
 
+    
+#Method to record a visit
+    def record_visit(self, visitor, resident):
+        # save visitor name + ID, and resident name
+        self.visits.append((visitor.name, visitor.IDno, resident.name))
+        print(f"Visit recorded: {visitor.name} (ID: {visitor.IDno}) visited {resident.name}")
+        
+        # Method to show all visits
+    def show_visits(self):
+        print(f"\nVisits at {self.name}:")
+        if not self.visits:
+            print("No visits yet.")
+        else:
+            for i, (visitor_name, visitor_id, resident_name) in enumerate(self.visits, start=1):
+                print(f"{i}. {visitor_name} (ID: {visitor_id}) visited {resident_name}")
+class Resident:
+    def __init__(self,name,room): 
+        self.name = name
+        self.roomNumber = room
+class visitor:
+    def __init__(self,name,IDno):
+        self.name = name
+        self.IDno = IDno
 
+hostel = Hostel("David's Ark Hostel", "Ankrah hill")
+
+resident1 = Resident("Gabriella", "C3")
+resident2 = Resident("Atu", "A14")
+
+visitor1 = visitor("Havy", "B2123")
+visitor2 = visitor("Daisy", "CV456")
+
+hostel.record_visit(visitor1, resident1)
+hostel.record_visit(visitor2, resident2)
+
+hostel.show_visits()
+
+        
+        
     
 
 
