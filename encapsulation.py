0f63898169f36075147293f5aa67d40f7d150943
Havy <hnakalyowa29@gmail.com>
2025-09-30 09:12:44 +0300

    additions on how it wrks

--- encapsulation.py
+++ encapsulation.py
@@ -3,5 +3,15 @@ class Student:
         self.name = name #public
         self._gpa = 3.5 #protected
         self.__password = "1234" #private
-        
+
+#creating an object of the class
+student1 = Student("Nakalyowa Hadijah")
+
+print(student1.name)
+print(student1._gpa)
+print(student1)
+
+
+
+
         
