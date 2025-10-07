be0d99a1423c3ebd25d8b94aba0a95bb4186eb18
Havy <hnakalyowa29@gmail.com>
2025-10-07 09:21:54 +0300

    new additions

--- piggy.py
+++ piggy.py
@@ -5,21 +5,24 @@ print("I have ",coins,"coins in my piggy")
 
 class Piggybank:
     def __init__(self,coins):
-        self.coins = coins
+        self._coins =coins
         self.put_in(coins)
     def put_in(self,amount):
         if amount <= 0:
             raise ValueError("Add real money")
-        self.coins += amount
+        self._coins += amount
 
     def take_out(self,amount):
         if amount <= 0:
             raise ValueError("Be real")
         if amount > self.coins:
             raise ValueError("Money is coming")
-        self.coins -= amount
+        self._coins -= amount
+    def how_much(self):
+        return self._coins
         
         
 
 Havy = Piggybank(10000)
-print("Havy's Box has",Havy.coins)
+Havy.put_in(5000)
+print("Havy's Box has", Havy.how_much(), "coins")
