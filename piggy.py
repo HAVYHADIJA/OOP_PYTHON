67f5478793ca3a559aac79f2d64b712f2e89ee8c
Havy <hnakalyowa29@gmail.com>
2025-10-07 08:48:23 +0300

    Changes

--- piggy.py
+++ piggy.py
@@ -2,3 +2,10 @@ coins = 100
 Jacob  = 500
 coins = 100 + 500
 print("I have ",coins,"coins in my piggy")
+
+class Piggybank:
+    def __init__(self,coins):
+        self.coins = coins
+        
+Havy = Piggybank(10000)
+print("Havy's Box has",Havy.coins)
