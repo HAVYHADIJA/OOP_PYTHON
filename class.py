f046e1c00b3e99d6b9b16348411f5972f724e724
Havy <hnakalyowa29@gmail.com>
2025-10-10 11:24:15 +0300

    my first code in file

--- class.py
+++ class.py
@@ -1,7 +1,18 @@
+## CLASS IN PYTHON WITHOUT ENCAPSULATION
 class ATM:
     def __init__(self,name,pin,account_balance):
         self.name = name
         self.pin = pin
         self.account_balance = account_balance
+        
+name = "Havy"
+pin = 124
+account_balance = 1000
+atm = ATM(name, pin, account_balance)
+print ("This ATM belongs to", atm.name) 
+print ("Your account balance is", atm.account_balance)
+
+
+
         
         
