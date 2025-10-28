fb228b953c046c298682652a2061a7923971e853
Havy <hnakalyowa29@gmail.com>
2025-10-28 08:45:26 +0300

    a class about inheritance and polymorphism

---
+++ class2.py
@@ -0,0 +1,15 @@
+class Gadget:
+    def __init__(self,name,price):
+        self.name = name
+        self.price = price
+    def start(self):
+        print("Gadget started")
+
+class Phone(Gadget):
+    def start(self):
+        print("Phone is starting")
+
+class Laptop(Gadget):
+    def start(self):
+        print("Laptop is starting")
+
