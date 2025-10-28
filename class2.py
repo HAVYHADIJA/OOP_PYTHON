64d8b011981f268fc9273cbc1d11f61a445973bf
Havy <hnakalyowa29@gmail.com>
2025-10-28 08:57:26 +0300

    new additions

--- class2.py
+++ class2.py
@@ -1,8 +1,6 @@
 class Gadget:
-    def __init__(self,name,price):
-        self.name = name
-        self.price = price
-    def start(self):
+    def __init__(self):
+       def start(self):
         print("Gadget started")
 
 class Phone(Gadget):
@@ -17,4 +15,16 @@ gadgets =[Phone(), Laptop()]
 
 for gadget in gadgets:
     gadget.start()
+
+class CameraFeature:
+    def take_photo(self):
+        print("Photo taken")
+
+class WifiEnabled:
+    def connect_wifi(self):
+        print("Connected to Wifi!")
+
+class Smartphone(Gadget,CameraFeature,WifiEnabled):
+    
+
 
