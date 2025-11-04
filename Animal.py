acaf4e012a0cecf51f00514fcbf12eaeba90ec89
Havy <hnakalyowa29@gmail.com>
2025-11-04 09:51:14 +0300

    overriding in progress

--- Animal.py
+++ Animal.py
@@ -1,12 +1,11 @@
 class Animal:
-    def __init__(self,name):
-        self.name = name
     def speak(self):
         print("The Animal is speaking")
 
 class Dog(Animal):
     def speak(self):
         print("The dog is barks")
+
 class Cat(Animal):
     def speak(self):
         print("The Cat Meows")
@@ -14,3 +13,23 @@ class Cat(Animal):
 Animal =[Dog(),Cat()]
 for a in Animal:
     a.speak()
+
+
+class Shape:
+    def calculate_area(self):
+        print("This is the area of the shape is 0")
+class Rectangle(Shape):
+    def __init__(self,):
+        super().__init__()
+    def calculate_area(self):
+        print()
+
+class Circle(Shape):
+    def calculate_area(self):
+        print()
+
+Shape =[Rectangle(),Circle()]
+for S in Shape:
+    S.calculate_area()
+
+
