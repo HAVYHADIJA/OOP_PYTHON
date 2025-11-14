22735790d9b9b1528cd057560a387e39139f6333
Havy <hnakalyowa29@gmail.com>
2025-11-14 11:24:08 +0300

    abstraction

--- Week_V.py
+++ Week_V.py
@@ -12,3 +12,33 @@ class GuildPresident(Student):
 people =[Student(),ClassRep(), GuildPresident()]
 for p in people:                    #same call different outputs
     p.greet()
+
+#Abstraction is hiding internal complexity. we only code simple methods
+#Abstract class(blueprint) help to design contracts or rules
+#Abstract methods, (with no code inside)
+
+from abc import ABC, abstractmethod #Helps us make an abstract base class
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+
+class Vehicle(ABC):
+    @abstractmethod
+    def display_info(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+    def displayInfo(self):
+        pass
+        
+class Car(Vehicle):
+    def __init__(self,num_doors):
+        self.num_doors = num_doors
+        super().__init__()
+
+    def display_Info(self):
+        print()
+    
