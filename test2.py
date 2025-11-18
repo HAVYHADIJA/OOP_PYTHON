703493d5b30ed1ef69fa2c606c1dd0e7c19dbe89
Havy <hnakalyowa29@gmail.com>
2025-11-18 08:36:16 +0300

    my personal practice

---
+++ test2.py
@@ -0,0 +1,14 @@
+class Bird:
+    def __init__(self,name,color,age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+all = []
+bird1 = Bird("Parrot","Blu",2)
+bird2 = Bird("Dove","White", 3)
+bird3 = Bird("Crestedcrane","Black",2)
+
+print(Bird.all)
+
+
