918f38074499c1c92e054eff21bb393d4767131f
Havy <hnakalyowa29@gmail.com>
2025-11-18 09:00:01 +0300

    datatypes

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+# a tuple is an ordered immutable list, cannot be changed
+# A list is a changable collection of items
+
+age = [23, 45, 12, 67, 34]
+age.append(27)
+print(age)
+
+age.remove(12)
+print(age)
+
