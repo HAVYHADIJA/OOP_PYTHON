4e32f475f26d5938082d085beaf92d3a056d1635
Havy <hnakalyowa29@gmail.com>
2025-11-25 08:26:05 +0300

    exam practice

---
+++ exam.py
@@ -0,0 +1,10 @@
+patient = "John Smith"
+age = 20
+is_new = True
+
+age = input("John,How old are you? ")
+print(f"You are {age} years old.")
+
+birth_year = int(input("Enter your birth year: "))
+age = 2025 - birth_year
+print(age)
