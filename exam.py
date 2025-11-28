736416817d0cf64a36ba393f054cdd35fe8fdd0e
Havy <hnakalyowa29@gmail.com>
2025-11-28 22:29:53 +0300

    PYTHON CONCEPTS

--- exam.py
+++ exam.py
@@ -18,3 +18,43 @@ course.lower()
 print(course.lower())
 
 print(course.replace('Beginners', 'Absolute Beginners'))
+
+#IF Statements
+temperature = 25
+if temperature > 30:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif temperature > 20:
+    print("Its a nice day")
+elif temperature > 10:
+    print("Its a cold day")
+else:
+    print("Its cold")
+
+#while loops
+i = 1
+while i <= 10:
+    print(i)
+    i = i + 1
+
+i = 1
+while i <= 10:
+    print(i * '*')
+    i = i + 1
+
+i = 100
+while i >= 1:
+    print(i)
+    i= i - 1
+
+# Lists
+names = ['John', 'Bob', 'Mosh', 'Sarah', 'Mary']
+print(names)
+print(names[0])
+print(names[-1])
+print(names[2:4])
+names[0] = 'Jon'
+print(names)
+
+
+
